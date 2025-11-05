a1074f7ae5b82ca743fb470a1609d4fac4007d1d
elslander <kjell.elslander@student.howest.be>
2025-11-05 11:59:19 +0100

    css fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kjellschool.github.io",
-  base: "https://github.com/KjellSchool/creative-code-club",
+  base: "creative-code-club",
 });
